bce7e310d29bad000c173bc3c42ccb2318fa599b
Rémi Berson <remi.berson@pm.me>
2023-02-25 19:46:14 +0100

    Leverage serialized engine to speed-up CLI

--- cli.js
+++ cli.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
 import path from 'node:path';
-import { existsSync, readFileSync } from 'node:fs';
-import { FiltersEngine, Request } from '@cliqz/adblocker';
+import { existsSync, readFileSync, writeFileSync } from 'node:fs';
+import { FiltersEngine, Request, ENGINE_VERSION } from '@cliqz/adblocker';
 
 import { BASE_PATH } from './scripts/helpers.js';
 
+const debug = process.env.DEBUG === 'true' ? console.log : () => {};
+
 (async () => {
+  const startTime = Date.now();
   const [, , url] = process.argv;
 
   if (!url) {
@@ -14,33 +17,52 @@ import { BASE_PATH } from './scripts/helpers.js';
     process.exit(1);
   }
 
+  // Generate input JSON if missing
   const trackerDBPath = path.join(BASE_PATH, 'dist', 'trackerdb.json');
-
   if (!existsSync(trackerDBPath)) {
-    // generate input JSON if missing
     await import('./scripts/export-json/index.js');
   }
 
-  const rawTrackerDB = JSON.parse(readFileSync(trackerDBPath, 'utf-8'));
-
-  const engine = FiltersEngine.fromTrackerDB(rawTrackerDB);
-
-  let matches = [];
-
-  if (url.startsWith('http')) {
-    const request = Request.fromRawDetails({ url });
-    matches = engine.getPatternMetadata(request);
-  } else {
-    matches = engine.metadata.fromDomain(url);
+  // Generate binary engine if missing
+  const trackerEnginePath = path.join(
+    BASE_PATH,
+    'dist',
+    `trackerdb_${ENGINE_VERSION}.engine`,
+  );
+  if (!existsSync(trackerEnginePath)) {
+    const rawTrackerDB = JSON.parse(readFileSync(trackerDBPath, 'utf-8'));
+    const engine = FiltersEngine.fromTrackerDB(rawTrackerDB);
+    writeFileSync(trackerEnginePath, engine.serialize());
   }
+
+  const loadingStart = Date.now();
+  const engine = FiltersEngine.deserialize(readFileSync(trackerEnginePath));
+  const loadingEnd = Date.now();
+
+  const matches = url.startsWith('http')
+    ? engine.getPatternMetadata(Request.fromRawDetails({ url }))
+    : engine.metadata.fromDomain(url);
+  const matchingEnd = Date.now();
+
+  debug('Timing:', {
+    init: loadingStart - startTime,
+    loading: loadingEnd - loadingStart,
+    matching: matchingEnd - loadingEnd,
+    total: matchingEnd - startTime,
+  });
 
   if (matches.length === 0) {
     console.log('No matches found');
   } else {
-    const results = {
-      url,
-      matches,
-    };
-    console.log(JSON.stringify(results, null, 2));
+    console.log(
+      JSON.stringify(
+        {
+          url,
+          matches,
+        },
+        null,
+        2,
+      ),
+    );
   }
 })();
