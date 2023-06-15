75830c3ad381b4ca2e60e48e8a1ef9a3cd10e137
Krzysztof Modras <chrmod@chrmod.net>
2023-06-15 12:57:56 +0200

    Ensure data model for whotracks.me

--- test/db/patterns.test.js
+++ test/db/patterns.test.js
@@ -70,9 +70,11 @@ test(RESOURCE_PATH, async (t) => {
         }
       });
 
-      await t.test('has at least one domain', () => {
+      await t.test('has at least one domain if not a alias', () => {
         const domains = spec.field('domains').optionalStringValue() || '';
-        assert.ok(domains.length > 0, `${specFile} has no domains`);
+        if (!spec.field('alias').optionalStringValue()) {
+          assert.ok(domains.length > 0, `${specFile} has no domains`);
+        }
       });
 
       await t.test('has valid filters', () => {
